8902ad447e6964d5603b44e62c88de9eacf8690e
kostadin kostadinov <kostadinovzk@gmail.com>
2016-10-11 11:14:35 +0300

    remove unused modifirs in interface variables

--- src/main/java/com/sap/test/httpparser/Status.java
+++ src/main/java/com/sap/test/httpparser/Status.java
@@ -1,7 +1,7 @@
 package com.sap.test.httpparser;
 
 public interface Status {
-	  public static final int BAD_REQUEST = 400;
-	  public static final int NOT_IMPLEMENTED = 501;
-	  public static final int OK = 200;
+	  final int BAD_REQUEST = 400;
+	  final int NOT_IMPLEMENTED = 501;
+	  final int OK = 200;
 }
