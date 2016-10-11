b4d9f4dfb83ef30dd40da649bea9078353338ac7
kostadin kostadinov <kostadinovzk@gmail.com>
2016-10-11 13:50:26 +0300

    added date header and non existing header tests

--- src/test/java/com/sap/test/httpparser/HttpParserUnitTest.java
+++ src/test/java/com/sap/test/httpparser/HttpParserUnitTest.java
@@ -1,17 +1,16 @@
 package com.sap.test.httpparser;
 
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.hamcrest.core.StringContains.containsString;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import org.junit.Test;
-
-import com.sap.test.httpparser.HttpParser;
-import com.sap.test.httpparser.Status;
 
 public class HttpParserUnitTest {
 	
@@ -26,7 +25,7 @@ public class HttpParserUnitTest {
 	}
 	
 	@Test 
-	public void testStstusLineStartingWithWhiteSpace() throws IOException{
+	public void testStatusLineStartingWithWhiteSpace() throws IOException{
 		httpRequestAsString = " GET / HTTP/1.1\r\n";
 		httpRequest = new HttpParser(new ByteArrayInputStream(httpRequestAsString.getBytes()));
 		assertThat(httpRequest.parseRequest(), is(Status.BAD_REQUEST));
@@ -113,6 +112,17 @@ public class HttpParserUnitTest {
 		assertEquals(httpRequest.getHeader("Host"), "localhost:50000");
 	}
 	
+	@Test
+	public void testNonExistingRequestHeaders() throws IOException{
+		httpRequestAsString = "GET / HTTP/1.1\r\n" +
+				"Host: localhost:50000\r\n" +
+				"Cookie: JSESSIONID=123456";
+		httpRequest = new HttpParser(new ByteArrayInputStream(httpRequestAsString.getBytes()));
+		httpRequest.parseRequest();
+		assertTrue(httpRequest.getHeaders().size() == 2);
+		assertEquals(httpRequest.getHeader("Cookies"), null);
+	}
+	
 	@Test 
 	public void testPostRequest() throws IOException{
 		httpRequestAsString = "POST / HTTP/1.1\r\n"	+
@@ -142,5 +152,10 @@ public class HttpParserUnitTest {
 		assertThat(httpRequest.compareVersion(2, 1), is(1));
 		assertThat(httpRequest.compareVersion(1, 2), is(1));
 		assertThat(httpRequest.compareVersion(1, 1), is(0));
+	}
+	
+	@Test
+	public void testDateHeader() throws IOException {
+		assertThat(HttpParser.getDateHeader(), allOf(containsString("Date:"), containsString("GMT")));
 	}
 }
