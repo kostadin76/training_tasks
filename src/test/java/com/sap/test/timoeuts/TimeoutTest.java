282c1d852908fafc64f6447d195664bb608cbdbd
kostadin kostadinov <kostadinovzk@gmail.com>
2016-10-11 09:25:42 +0300

    ignoring timeouters test

--- src/test/java/com/sap/test/timoeuts/TimeoutTest.java
+++ src/test/java/com/sap/test/timoeuts/TimeoutTest.java
@@ -2,6 +2,7 @@ package com.sap.test.timoeuts;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
@@ -24,12 +25,14 @@ public class TimeoutTest {
     @Rule
     public Timeout globalTimeout = Timeout.seconds(10); // 10 seconds max per method tested
 
+    @Ignore("")
     @Test
     public void testSleepForTooLong() throws Exception {
         log += "ran1";
         TimeUnit.SECONDS.sleep(100); // sleep for 100 seconds
     }
 
+    @Ignore
     @Test
     public void testBlockForever() throws Exception {
         log += "ran2";
