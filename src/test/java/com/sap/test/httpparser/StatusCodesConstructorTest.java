754fd3c93f34c8fefb76cf5b8453148f5989ef84
kostadin kostadinov <kostadinovzk@gmail.com>
2016-10-11 12:16:57 +0300

    added test for StatusCodes check

---
+++ src/test/java/com/sap/test/httpparser/StatusCodesConstructorTest.java
@@ -0,0 +1,10 @@
+package com.sap.test.httpparser;
+
+import org.junit.Test;
+
+public class StatusCodesConstructorTest {
+	@Test
+	public void testStatusCodesConstructor(){
+		new StatusCodes();
+	}
+}
